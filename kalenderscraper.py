6b34416e01fa029c799d5b4c20a539a50ed43b92
derchris <derchris@me.com>
2016-03-23 11:32:23 +0100

    use requests(urllib3) and html5lib parser to scrape wiki

--- kalenderscraper.py
+++ kalenderscraper.py
@@ -4,56 +4,15 @@
 
 import os
 import datetime
-import urllib2
+import requests
 from bs4 import BeautifulSoup
 from icalendar import Calendar, Event
 import json
 import re
 from itertools import groupby
 
-DayL = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+requests.packages.urllib3.disable_warnings()
 
-now = datetime.datetime.now()
-
-# Scrape calender data from wiki
-soup = BeautifulSoup(urllib2.urlopen('http://wiki.muc.ccc.de/kalender').read(), "html.parser")
-rows = soup.find("table").find_all('tr')
-
-dates = []
-
-pttrn = re.compile(ur'[\d]{1,2}.[\d]{1,2}.')
-
-def match_class(target):
-    def do_match(tag):
-        classes = tag.get('class', [])
-        return all(c in classes for c in target)
-    return do_match
-
-for row in rows:
-    try:
-        data = row.find_all("td")
-
-        d_date  = re.findall(pttrn, data[0].get_text())[0]
-        d_day   = d_date.split('.')[0]
-        d_month = d_date.split('.')[1]
-        d_time  = data[1].get_text().strip(' ')
-        d_name  = data[2].get_text().strip(' ')
-        d_link_raw = data[2].find(match_class(["urlextern"]))
-        if d_link_raw:
-            d_link = d_link_raw.get('href')
-        d_link_raw = data[2].find(match_class(["wikilink1"]))
-        if d_link_raw:
-            d_link = 'https://wiki.muc.ccc.de' + d_link_raw.get('href')
-        d_public= data[3].get_text().strip(' ')
-        d_anzahl= data[4].get_text().strip(' ')
-        d_keyholder = data[5].get_text().strip(' ')
-
-        dates.append((d_day, d_month, d_time, d_name,
-                    d_public, d_anzahl, d_keyholder, d_link))
-        d_link = u''
-
-    except:
-        pass
 
 def accumulate(l):
     for key, group in groupby(l, key=lambda x: '%s:%s' % (x[3], x[2])):
@@ -64,6 +23,51 @@ def accumulate(l):
                 date_occurence = data[0]
             event_occurence+=1
         yield (date_occurence, data[1], data[2], key.split(':')[0], data[4], data[5], data[6], data[7], event_occurence)
+
+
+def match_class(target):
+    def do_match(tag):
+        classes = tag.get('class', [])
+        return all(c in classes for c in target)
+    return do_match
+
+
+DayL = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+now = datetime.datetime.now()
+
+# Scrape calender data from wiki
+soup = BeautifulSoup(requests.get('https://wiki.muc.ccc.de/kalender', verify=False).text, 'html5lib')
+table = soup.find('table', attrs={'class': 'inline'})
+table_body = table.find('tbody')
+rows = table_body.find_all('tr')
+
+dates = []
+
+pttrn = re.compile(ur'[\d]{1,2}.[\d]{1,2}.')
+
+for row in rows:
+    data = row.find_all("td")
+
+    d_date  = re.findall(pttrn, data[0].get_text())[0]
+    d_day   = d_date.split('.')[0]
+    d_month = d_date.split('.')[1]
+    d_time  = data[1].get_text().strip(' ')
+    d_name  = data[2].get_text().strip(' ')
+    d_link_raw = data[2].find(match_class(["urlextern"]))
+    if d_link_raw:
+        d_link = d_link_raw.get('href')
+    d_link_raw = data[2].find(match_class(["wikilink1"]))
+    if d_link_raw:
+        d_link = 'https://wiki.muc.ccc.de' + d_link_raw.get('href')
+    d_public= data[3].get_text().strip(' ')
+    d_anzahl= data[4].get_text().strip(' ')
+    d_keyholder = data[5].get_text().strip(' ')
+
+    dates.append((d_day, d_month, d_time, d_name,
+                d_public, d_anzahl, d_keyholder, d_link))
+    d_link = u''
+
 
 # Export ics file with all dates
 cal = Calendar()
