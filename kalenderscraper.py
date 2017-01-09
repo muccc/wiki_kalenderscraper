6206c27065ca6b20410297ab36dc97fe307162f8
chris007de <mail@chris007.de>
2017-01-09 20:13:21 +0100

    pleasing pep8

--- kalenderscraper.py
+++ kalenderscraper.py
@@ -21,8 +21,9 @@ def accumulate(l):
         for i, data in enumerate(group):
             if i == 0:
                 date_occurence = data[0]
-            event_occurence+=1
-        yield (date_occurence, data[1], data[2], key.split(':')[0], data[4], data[5], data[6], data[7], event_occurence, data[8])
+            event_occurence += 1
+        yield (date_occurence, data[1], data[2], key.split(':')[0], data[4],
+               data[5], data[6], data[7], event_occurence, data[8])
 
 
 def match_class(target):
@@ -32,12 +33,14 @@ def match_class(target):
     return do_match
 
 
-DayL = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+DayL = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+        'Saturday', 'Sunday']
 
 now = datetime.datetime.now()
 
 # Scrape calender data from wiki
-soup = BeautifulSoup(requests.get('https://wiki.muc.ccc.de/kalender', verify=False).text, 'html5lib')
+soup = BeautifulSoup(requests.get('https://wiki.muc.ccc.de/kalender',
+                     verify=False).text, 'html5lib')
 table = soup.find('table', attrs={'class': 'inline'})
 table_body = table.find('tbody')
 rows = table_body.find_all('tr')
@@ -51,18 +54,18 @@ for row in rows:
     data = row.find_all("td")
 
     # If there is no valid date found, we skip this entry
-    d_date  = re.findall(pttrn_date, data[0].get_text().strip(' '))
+    d_date = re.findall(pttrn_date, data[0].get_text().strip(' '))
     if len(d_date) == 1:
         d_date = d_date[0]
     else:
         continue
 
-    d_day   = d_date.split('.')[0]
+    d_day = d_date.split('.')[0]
     d_month = d_date.split('.')[1]
 
     # If there is no valid time given, we assume a whole-day event
     cell_time = data[1].get_text().strip(' ')
-    d_time  = re.findall(pttrn_date, cell_time)
+    d_time = re.findall(pttrn_date, cell_time)
     if len(d_time) == 1:
         d_time = d_time[0]
         d_duration_h = 2
@@ -70,19 +73,19 @@ for row in rows:
         d_time = "00:00"
         d_duration_h = 24
 
-    d_name  = data[2].get_text().strip(' ')
+    d_name = data[2].get_text().strip(' ')
     d_link_raw = data[2].find(match_class(["urlextern"]))
     if d_link_raw:
         d_link = d_link_raw.get('href')
     d_link_raw = data[2].find(match_class(["wikilink1"]))
     if d_link_raw:
         d_link = 'https://wiki.muc.ccc.de' + d_link_raw.get('href')
-    d_public= data[3].get_text().strip(' ')
-    d_anzahl= data[4].get_text().strip(' ')
+    d_public = data[3].get_text().strip(' ')
+    d_anzahl = data[4].get_text().strip(' ')
     d_keyholder = data[5].get_text().strip(' ')
 
     dates.append((d_day, d_month, d_time, d_name,
-                d_public, d_anzahl, d_keyholder, d_link, d_duration_h))
+                 d_public, d_anzahl, d_keyholder, d_link, d_duration_h))
     d_link = u''
 
 
@@ -102,8 +105,9 @@ for date in accumulate(dates):
     event.add('summary', date[3])
 
     if date[8] == 1 and date[9] < 24:
-        dtstart = datetime.datetime.strptime(date[0] + "." + date[1] + "." +
-                    str(now.year) + " " + date[2], "%d.%m.%Y %H:%M")
+        datestring = date[0] + "." + date[1] + "." + \
+                     str(now.year) + " " + date[2]
+        dtstart = datetime.datetime.strptime(datestring, "%d.%m.%Y %H:%M")
         event.add('dtstart', dtstart)
         # just default duration of x hours for the each event
         event.add('dtend',   dtstart+datetime.timedelta(hours=d_duration_h))
@@ -145,8 +149,8 @@ for date in accumulate(dates):
     # Implementations MUST be able to receive and persist values of at least
     # 255 characters for this property.
     uid = 'wikical' + date[0] + '.' + date[1] + '.' \
-            + date[3].replace(" ","") + '.' \
-            + date[2] + '@api.muc.ccc.de'
+          + date[3].replace(" ", "") + '.' \
+          + date[2] + '@api.muc.ccc.de'
     uid = uid.replace(':', '.')
 
     event.add('uid', uid)
@@ -169,27 +173,35 @@ fhandle.close()
 # Export next event to JSON file
 for date in accumulate(dates):
     if date[8] > 1:
-        testdate = datetime.datetime.strptime(date[0] + "." + date[1] + "." + \
-                    str(now.year) + " 00:00" , "%d.%m.%Y %H:%M")
+        datestring = date[0] + "." + date[1] + "." + \
+            str(now.year) + " 00:00"
+        testdate = datetime.datetime.strptime(datestring, "%d.%m.%Y %H:%M")
     else:
-        testdate = datetime.datetime.strptime(date[0] + "." + date[1] + "." + \
-                    str(now.year) + " " + date[2], "%d.%m.%Y %H:%M")
+        datestring = date[0] + "." + date[1] + "." + \
+            str(now.year) + " " + date[2]
+        testdate = datetime.datetime.strptime(datestring, "%d.%m.%Y %H:%M")
 
     if testdate > now:
-        eventname = date[3].replace(u'ü', 'ue').replace(u'ä', 'ae').replace(u'ö', 'oe')
+        eventname = date[3].replace(u'ü', 'ue')
+        eventname = eventname.replace(u'ä', 'ae')
+        eventname = eventname.replace(u'ö', 'oe')
+
         if date[8] > 1:
             event_end = int(date[0]) + int((int(date[8]) - 1))
-            jsonstring = json.dumps({'date': date[0] + "." + date[1] + ".-" + str(event_end) + "." + date[1] + "." , \
-                                     'time': '10:00', \
-                                     'weekday': DayL[testdate.weekday()], \
-                                     'name': eventname, \
+            datestring = date[0] + "." + date[1] + ".-" + \
+                str(event_end) + "." + date[1] + "."
+            jsonstring = json.dumps({'date': datestring,
+                                     'time': '10:00',
+                                     'weekday': DayL[testdate.weekday()],
+                                     'name': eventname,
                                      'public': date[4]})
 
         else:
-            jsonstring = json.dumps({'date': date[0] + "." + date[1] + ".", \
-                                     'time': date[2], \
-                                     'weekday': DayL[testdate.weekday()], \
-                                     'name': eventname, \
+            datestring = date[0] + "." + date[1] + "."
+            jsonstring = json.dumps({'date': datestring,
+                                     'time': date[2],
+                                     'weekday': DayL[testdate.weekday()],
+                                     'name': eventname,
                                      'public': date[4]})
 
         path = os.path.dirname(os.path.realpath(__file__))
